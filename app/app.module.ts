4077fc4e7305adb1699b2b37cf4125089500a1d9
Denny Biasiolli <denny.biasiolli@gmail.com>
2016-12-24 10:24:16 +0100

    using new components in module

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PairingComponent } from './pairing.component';
+import { AboutComponent } from './about.component';
 
 @NgModule({
     bootstrap: [ AppComponent ],
-    imports: [ BrowserModule ],
-    declarations: [ AppComponent ],
+    imports: [ 
+        BrowserModule,
+    ],
+    declarations: [
+        AppComponent,
+        PairingComponent,
+        AboutComponent,
+    ],
 })
 export class AppModule { }
