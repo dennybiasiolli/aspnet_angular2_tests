0400d852ff3ab05bf902f3a62f9767dd1052ee78
Denny Biasiolli <denny.biasiolli@gmail.com>
2016-12-24 16:14:45 +0100

    adding angular2 routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PairingComponent } from './pairing.component';
 import { AboutComponent } from './about.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/pairing', pathMatch: 'full' },
+  { path: 'pairing',  component: PairingComponent },
+  { path: 'about', component: AboutComponent },
+];
+
 @NgModule({
     bootstrap: [ AppComponent ],
     imports: [ 
         BrowserModule,
+        RouterModule.forRoot(routes),
     ],
     declarations: [
         AppComponent,
