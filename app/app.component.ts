8e06d1a91a0948fbe335b50ddc1805edeb1c9f66
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2016-12-24 10:24:16 +0100

    using new components in module

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,15 +4,11 @@ import { Component } from '@angular/core';
     moduleId: module.id,
     selector: 'my-app',
     template: `
-        <input type="text" [value]="food" (input)="foodInput($event)" />
-        <p>Sriracha sauce is great with {{ food }}</p>
+        <nav>
+            <a href="/pairing">Pairing</a> |
+            <a href="/about">About</a>
+        </nav>
+        <my-pairing></my-pairing>
     `,
 })
-export class AppComponent {
-    food = 'kielbasa';
-
-    foodInput(event: Event) {
-        const target = event.target as HTMLInputElement;
-        this.food = target.value;
-    }
-}
+export class AppComponent { }
