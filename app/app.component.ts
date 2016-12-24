047f075252a53ad3e759831eccea249c32566344
Denny Biasiolli <denny.biasiolli@gmail.com>
2016-12-24 09:31:00 +0100

    template: updating property from input

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,10 +4,15 @@ import { Component } from '@angular/core';
     moduleId: module.id,
     selector: 'my-app',
     template: `
-        <input type="text" [value]="food" />
+        <input type="text" [value]="food" (input)="foodInput($event)" />
         <p>Sriracha sauce is great with {{ food }}</p>
     `,
 })
 export class AppComponent {
     food = 'kielbasa';
+
+    foodInput(event: Event) {
+        const target = event.target as HTMLInputElement;
+        this.food = target.value;
+    }
 }
